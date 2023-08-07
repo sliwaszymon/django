893faca9edafdeb7ac283a4cd8c30ae0a2c29b0a
Szymon Ś <s.sliwa@wamasoft.pl>
2023-08-07 16:59:14 +0200

    started with deleting and it works but modals with confirmation didnt pop up

--- homesite/todos/urls.py
+++ homesite/todos/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import TodosIndexView
+from .views import TodosIndexView, TodoDeleteView
 
 urlpatterns = [
     path("", TodosIndexView.as_view(), name="todos-index"),
+    path("todo/<pk>/delete/", TodoDeleteView.as_view(), name="todo-delete")
 ]
